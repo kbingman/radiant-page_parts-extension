6de55f6f442ce4f18b7916ae823378016fdcdf20
Josh French <josh@digitalpulp.com>
2010-08-15 12:36:46 -0400

    Modify PagePartsController without replacing it completely

--- page_parts_extension.rb
+++ page_parts_extension.rb
@@ -8,6 +8,7 @@ class PagePartsExtension < Radiant::Extension
   def activate
     PagePart.send(:include, PageParts::PagePartExtensions)
     Page.send(:include, PageParts::PageExtensions, PageParts::PagePartTags)
+    Admin::PagePartsController.send(:include, PageParts::ControllerExtensions)
     Admin::ResourceController.prepend_view_path 'app/views'
     Admin::PagesController.helper PageParts::PagePartHelper
     ActiveSupport::Dependencies.load_paths << File.join(Rails.root, %w(app models))
